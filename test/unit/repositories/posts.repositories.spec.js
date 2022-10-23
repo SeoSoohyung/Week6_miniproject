51d525df4929e5bdc19dd6605adca85edece85f9
LAPTOP-PQPCKK5H\alstj <alstjq123579@daum.net>
2022-10-23 19:02:16 +0900

    김민섭: postsRepo test코드 작성 중

--- test/unit/repositories/posts.repositories.spec.js
+++ test/unit/repositories/posts.repositories.spec.js
@@ -1,24 +1,5 @@
 //게시글
-const jest = require("jest");
-const PostsRepository = require("../../../repository/posts");
 
-const mockPostModel = () => ({
-  findAll: jest.fn(),
-  findOne: jest.fn(),
-  create: jest.fn(),
-  delete: jest.fn(),
-});
-describe("posts Repository Layer Test", () => {
-  let postsRepository = new PostsRepository();
-  postsRepository.Posts = mockPostModel();
-
-  beforeEach(() => {
-    jest.resetAllMock();
-  });
-
-  test("Posts Repository Method findAllPost", async () => {
-    const posts = await postsRepository.findAllPost({});
-    expect(posts).toBeUndefinde();
-    console.log("Posts", posts);
-  });
+test("two plus two is four", () => {
+  expect(2 + 2).toBe(4);
 });
